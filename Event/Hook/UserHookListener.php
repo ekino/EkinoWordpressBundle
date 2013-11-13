1361aeff0e74381f206e2d1e5036feca6a40fd1f
Vincent Composieux <composieux@ekino.com>
2013-11-13 17:58:51 +0100

    Typo fix 2011 to 2013

--- Event/Hook/UserHookListener.php
+++ Event/Hook/UserHookListener.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Ekino Wordpress package.
  *
- * (c) 2011 Ekino
+ * (c) 2013 Ekino
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
