8ba8b4009efa748b1b3f281e119ef31b37d1a32f
Xavier Coureau <xav@takeatea.com>
2015-02-18 22:17:06 +0100

    Removed code causing invalid response headers
    Added helper to allow tag and category link generation
    
    Revert "Added helper to allow tag and category link generation"
    
    This reverts commit 394fcc2af856f7c0aae2afdc6fcf091f2d9f4b87.

--- Tests/Event/Subscriber/WordpressResponseSubscriberTest.php
+++ Tests/Event/Subscriber/WordpressResponseSubscriberTest.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class WordpressResponseSubscriberTest extends \PHPUnit_Framework_TestCase
 {
     protected $event;
-    protected $request;
     protected $response;
     protected $wordpress;
 
@@ -30,10 +29,9 @@ class WordpressResponseSubscriberTest extends \PHPUnit_Framework_TestCase
     {
         $this->event = $this->getMockBuilder('Symfony\Component\HttpKernel\Event\FilterResponseEvent')->disableOriginalConstructor()->getMock();
         $this->response = $this->getMockBuilder('Ekino\WordpressBundle\Wordpress\WordpressResponse')->disableOriginalConstructor()->getMock();
-        $this->request = $this->getMockBuilder('Symfony\Component\HttpFoundation\Request')->disableOriginalConstructor()->getMock();
         $this->wordpress = $this->getMockBuilder('Ekino\WordpressBundle\Wordpress\Wordpress')->disableOriginalConstructor()->getMock();
 
-        $this->subscriber = new WordpressResponseSubscriber(array($this, 'getHeadersMock'), $this->wordpress);
+        $this->subscriber = new WordpressResponseSubscriber($this->wordpress);
     }
 
     public function testGetSubscribedEvents()
@@ -43,11 +41,6 @@ class WordpressResponseSubscriberTest extends \PHPUnit_Framework_TestCase
         );
 
         $this->assertEquals($expected, WordpressResponseSubscriber::getSubscribedEvents());
-    }
-
-    public function testGetHttpHeadersCallback()
-    {
-        $this->assertEquals(array($this, 'getHeadersMock'), $this->subscriber->getHttpHeadersCallback());
     }
 
     public function testOnKernelResponseNoWordpressResponse()
@@ -92,106 +85,12 @@ class WordpressResponseSubscriberTest extends \PHPUnit_Framework_TestCase
             ->method('getWpQuery')
             ->will($this->returnValue(null));
 
-        $this->request->expects($this->never())
-            ->method('getUri');
-
         $this->subscriber->onKernelResponse($this->event);
-    }
-
-    public function testOnKernelResponseNo404()
-    {
-        $this->event->expects($this->once())
-            ->method('getRequest')
-            ->will($this->returnValue($this->request));
-        $this->event->expects($this->once())
-            ->method('getResponse')
-            ->will($this->returnValue($this->response));
-
-        $this->event->expects($this->once())
-            ->method('getRequestType')
-            ->will($this->returnValue(HttpKernelInterface::MASTER_REQUEST));
-
-        $this->wordpress->expects($this->once())
-            ->method('getWpQuery')
-            ->will($this->returnValue(new WP_QueryMock()));
-        $this->request->expects($this->once())
-            ->method('getUri')
-            ->will($this->returnValue('http://wwww.test.com/random-test'));
-
-        $this->subscriber->onKernelResponse($this->event);
-    }
-
-    public function testOnKernelResponsePushHeader()
-    {
-        $subscriber = new WordpressResponseSubscriber(array($this, 'getHavingHeadersMock'), $this->wordpress);
-
-        $this->event->expects($this->once())
-            ->method('getRequest')
-            ->will($this->returnValue($this->request));
-        $this->event->expects($this->once())
-            ->method('getResponse')
-            ->will($this->returnValue($this->response));
-        $this->event->expects($this->once())
-            ->method('getRequestType')
-            ->will($this->returnValue(HttpKernelInterface::MASTER_REQUEST));
-        $this->wordpress->expects($this->once())
-            ->method('getWpQuery')
-            ->will($this->returnValue(new WP_QueryMock(true)));
-
-        $parameterBag = $this->getMock('Symfony\Component\HttpFoundation\ParameterBag');
-        $this->response->headers = $parameterBag;
-        $parameterBag->expects($this->once())
-            ->method('set')
-            ->with($this->equalTo('test-header'), $this->equalTo('is working'));
-
-        $this->request->expects($this->once())
-            ->method('getUri')
-            ->will($this->returnValue('http://wwww.test.com/random-test'));
-        $this->response->expects($this->once())
-            ->method('setStatusCode')
-            ->with($this->equalTo(404));
-
-        $subscriber->onKernelResponse($this->event);
-    }
-
-    public function testOnKernelResponseNoPushCacheHeader()
-    {
-        $subscriber = new WordpressResponseSubscriber(array($this, 'getHavingHeadersCacheMock'), $this->wordpress);
-
-        $this->event->expects($this->once())
-            ->method('getRequest')
-            ->will($this->returnValue($this->request));
-        $this->event->expects($this->once())
-            ->method('getResponse')
-            ->will($this->returnValue($this->response));
-        $this->event->expects($this->once())
-            ->method('getRequestType')
-            ->will($this->returnValue(HttpKernelInterface::MASTER_REQUEST));
-        $this->wordpress->expects($this->once())
-            ->method('getWpQuery')
-            ->will($this->returnValue(new WP_QueryMock(true)));
-
-        $parameterBag = $this->getMock('Symfony\Component\HttpFoundation\ParameterBag');
-        $this->response->headers = $parameterBag;
-        $parameterBag->expects($this->never())
-            ->method('set');
-
-        $this->request->expects($this->once())
-            ->method('getUri')
-            ->will($this->returnValue('http://wwww.test.com/random-test'));
-        $this->response->expects($this->once())
-            ->method('setStatusCode')
-            ->with($this->equalTo(404));
-
-        $subscriber->onKernelResponse($this->event);
     }
 
     public function testOnKernelResponse()
     {
         $this->event->expects($this->once())
-            ->method('getRequest')
-            ->will($this->returnValue($this->request));
-        $this->event->expects($this->once())
             ->method('getResponse')
             ->will($this->returnValue($this->response));
         $this->event->expects($this->once())
@@ -201,48 +100,11 @@ class WordpressResponseSubscriberTest extends \PHPUnit_Framework_TestCase
             ->method('getWpQuery')
             ->will($this->returnValue(new WP_QueryMock(true)));
 
-        $this->request->expects($this->once())
-            ->method('getUri')
-            ->will($this->returnValue('http://wwww.test.com/random-test'));
         $this->response->expects($this->once())
             ->method('setStatusCode')
             ->with($this->equalTo(404));
 
         $this->subscriber->onKernelResponse($this->event);
-    }
-
-    /**
-     * @param string $uri
-     *
-     * @return array
-     */
-    public function getHeadersMock($uri)
-    {
-        return array();
-    }
-
-    /**
-     * @param string $uri
-     *
-     * @return array
-     */
-    public function getHavingHeadersMock($uri)
-    {
-        return array(
-            'test-header' => 'is working',
-        );
-    }
-
-    /**
-     * @param string $uri
-     *
-     * @return array
-     */
-    public function getHavingHeadersCacheMock($uri)
-    {
-        return array(
-            'cache-control' => 'should not work',
-        );
     }
 }
 
