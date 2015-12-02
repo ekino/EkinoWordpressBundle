9173916f100a14b30f095919df217e55b8c813cb
guillaume leclercq <guillaume.leclercq@fullsix.com>
2015-12-02 17:35:37 +0100

    add wp_get_attachment_url with test

--- Tests/Twig/Extension/PostMetaExtensionTest.php
+++ Tests/Twig/Extension/PostMetaExtensionTest.php
@@ -7,56 +7,80 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-namespace Ekino\WordpressBundle\Tests\Twig\Extension;
-
-use Ekino\WordpressBundle\Twig\Extension\PostMetaExtension;
-
-/**
- * Class PostMetaExtensionTest
- *
- * @author Xavier Coureau <xav.is@2cool4school.fr>
- */
-class PostMetaExtensionTest extends \PHPUnit_Framework_TestCase
-{
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $postMetaManager;
-
-    /**
-     * @var PostMetaExtension
-     */
-    protected $extension;
-
-    protected function setUp()
+namespace {
+    function wp_get_attachment_url($imgId)
     {
-        if (!class_exists('\Twig_Extension')) {
-            $this->markTestSkipped('Twig is not enabled');
+        return 'http://www.exemple.com/image.jpg';
+    }
+}
+namespace Ekino\WordpressBundle\Tests\Twig\Extension {
+
+    use Ekino\WordpressBundle\Twig\Extension\PostMetaExtension;
+
+    /**
+     * Class PostMetaExtensionTest
+     *
+     * @author Xavier Coureau <xav.is@2cool4school.fr>
+     */
+    class PostMetaExtensionTest extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * @var \PHPUnit_Framework_MockObject_MockObject
+         */
+        protected $postMetaManager;
+
+        /**
+         * @var PostMetaExtension
+         */
+        protected $extension;
+
+        protected function setUp()
+        {
+            if (!class_exists('\Twig_Extension')) {
+                $this->markTestSkipped('Twig is not enabled');
+            }
+
+            $this->postMetaManager = $this->getMockBuilder('Ekino\WordpressBundle\Manager\PostMetaManager')->disableOriginalConstructor()->getMock();
+            $this->extension = new PostMetaExtension($this->postMetaManager);
         }
 
-        $this->postMetaManager = $this->getMockBuilder('Ekino\WordpressBundle\Manager\PostMetaManager')->disableOriginalConstructor()->getMock();
-        $this->extension = new PostMetaExtension($this->postMetaManager);
-    }
+        /**
+         *   Get test name
+         */
+        public function testGetName()
+        {
+            $this->assertEquals('ekino_wordpress_post_meta', $this->extension->getName());
+        }
 
-    public function testGetName()
-    {
-        $this->assertEquals('ekino_wordpress_post_meta', $this->extension->getName());
-    }
+        /**
+         *   Get test functions
+         */
+        public function testGetFunctions()
+        {
+            $this->assertContainsOnly('\Twig_SimpleFunction', $this->extension->getFunctions());
+        }
 
-    public function testGetFunctions()
-    {
-        $this->assertContainsOnly('\Twig_SimpleFunction', $this->extension->getFunctions());
-    }
+        /**
+         * Check the correct result for an existing option
+         */
+        public function testGetPostMeta()
+        {
+            $postMeta = $this->getMock('Ekino\WordpressBundle\Entity\PostMeta');
+            $this->postMetaManager->expects($this->once())
+                ->method('getPostMeta')
+                ->with($this->equalTo(12), $this->equalTo('meta-test'), $this->equalTo(true))
+                ->will($this->returnValue($postMeta));
 
-    /**
-     * Check the correct result for an existing option
-     */
-    public function testGetPostMeta()
-    {
-        $this->postMetaManager->expects($this->once())
-            ->method('getPostMeta');
+            $result = $this->extension->getPostMeta(12, 'meta-test', true);
+            $this->assertEquals($postMeta, $result);
+        }
 
-        $this->extension->getPostMeta(12, 'meta-test', true);
+        /**
+         *  Check getImageUrlFromId method
+         */
+        public function testGetImageUrlFromId()
+        {
+            $this->assertEquals('http://www.exemple.com/image.jpg', $this->extension->getImageUrlFromId(1));
+        }
     }
 }
